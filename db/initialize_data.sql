29fcd46c625ec1d4ac89a26e076255f033226503
Huy Tran <baohuy82.th@gmail.com>
2025-05-07 01:47:54 -0700

    Update

---
+++ db/initialize_data.sql
@@ -0,0 +1,5 @@
+USE genetic_db;
+INSERT INTO Organisms (scientific_name, common_name, taxonomy, genome_size) VALUES
+ ('Homo sapiens','Human','Eukaryota;Metazoa;Chordata;Mammalia;Primates',3300000000),
+ ('Escherichia coli','E. coli','Bacteria;Proteobacteria;Gammaproteobacteria',4600000),
+ ('Saccharomyces cerevisiae','Baker’s yeast','Eukaryota;Fungi;Ascomycota',12157105);
